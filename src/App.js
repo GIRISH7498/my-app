b31a8f964eb3e6e0e80c14c0290b01c597918f6c
Girish Giradkar <girishgiradkar824@gmail.com>
2022-12-20 23:56:11 +0530

    btn-keyup

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
+import { useState } from "react";
+
 function App() {
-    let clickMe1 = () => {
-      console.log("Hello");
-    };
-  
-    let clickMe2 = (e) => {
-      console.log(e);
-    };
-  
-    // Custom Parameter :: using arrow funcn
-    let clickMe3 = (p1) => {
-      console.log(p1);
-    };
-  
-    let clickMe4 = (e, p1) => {
-      console.log(e, p1);
-    };
-  
-    return (
-      <div>
-        <h1>Btn Click Demo</h1>
-  
-        {/** Default No param */}
-        <input type="button" value="Click Me 1" onClick={clickMe1} />
-  
-        {/** Default Event Param */}
-        <input type="button" value="Click Me 2" onClick={clickMe2} />
-  
-        {/** Custom Param */}
-        <input
-          type="button"
-          value="Click Me 3"
-          onClick={() => clickMe3("primary")}
-        />
-        <input
-          type="button"
-          value="Click Me 3"
-          onClick={() => clickMe3("danger")}
-        />
-  
-        {/** Event + Custom Param */}
-        <input
-          type="button"
-          value="Click Me 4"
-          onClick={(e) => clickMe4(e, "adsfaf adsf")}
-        />
-      </div>
-    );
-  }
-  
-  export default App;
-  
+  let [message, setMessage] = useState("Abc");
+
+  let updateMessage = (e) => {
+    // e.target == document.querySelector("input")
+    message = e.target.value;
+    setMessage(message);
+  };
+
+  return (
+    <div>
+      <h1>Working with Input</h1>
+
+      <input
+        type="text"
+        placeholder="Enter message"
+        value={message}
+        onChange={updateMessage}
+      />
+
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
